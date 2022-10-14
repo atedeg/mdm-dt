d7083acba694f664ec009a1aba83ce323e6d0ad9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-10-14 14:48:52 +0200

    feat: add ph alarm handler

--- alarm-manager/src/main/scala/dev/atedeg/mdm/api/Handlers.scala
+++ alarm-manager/src/main/scala/dev/atedeg/mdm/api/Handlers.scala
@@ -3,27 +3,36 @@ package dev.atedeg.mdm.api
 import java.time.LocalTime
 
 import cats.Monad
+import cats.effect.IO
 import cats.effect.LiftIO
-import cats.syntax.all.*
 import cats.implicits.*
+import cats.syntax.all.*
 import eu.timepit.refined.numeric.Greater
 import eu.timepit.refined.refineV
 
+import dev.atedeg.mdm.{ managePhAlarm, manageTemperatureAlarm }
 import dev.atedeg.mdm.IncomingEvents.*
 import dev.atedeg.mdm.TemperatureFailure
 import dev.atedeg.mdm.TemperatureFailureMessage
-import dev.atedeg.mdm.manageTemperatureAlarm
+import dev.atedeg.mdm.dto.*
+import dev.atedeg.mdm.utils.monads.*
 import dev.atedeg.mdm.utils.monads.{ CanRaise, Emits }
 import dev.atedeg.mdm.utils.monads.getOrRaise
 import dev.atedeg.mdm.utils.serialization.DTOOps.*
-import dev.atedeg.mdm.utils.monads.*
-import dev.atedeg.mdm.dto.*
-import cats.effect.IO
+import dev.atedeg.mdm.PhFailure
 
 def handleTemperatureOutOfRangeEvent[M[_]: Monad: LiftIO: CanRaise[String]](event: TemperatureOutOfRangeDTO): M[Unit] =
   for
     r <- validate(event)
     message = manageTemperatureAlarm(TemperatureFailure(LocalTime.now(), r.temperature, r.device))
+    _ <- IO.println("Sending an e-mail to the admin...").liftIO[M]
+    _ <- IO.println(message).liftIO[M]
+  yield ()
+
+def handlePhOutOfRangeEvent[M[_]: Monad: LiftIO: CanRaise[String]](event: PhOutOfRangeDTO): M[Unit] =
+  for
+    r <- validate(event)
+    message = managePhAlarm(PhFailure(LocalTime.now(), r.ph, r.device))
     _ <- IO.println("Sending an e-mail to the admin...").liftIO[M]
     _ <- IO.println(message).liftIO[M]
   yield ()
